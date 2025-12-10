d51e96fc02e54791d93f0a2ca5208637979e4574
thewiserdome-star <thewiserdome@gmail.com>
2025-12-10 20:12:36 +0530

    Update tailwind.config.js

--- react-app/tailwind.config.js
+++ react-app/tailwind.config.js
@@ -3,6 +3,8 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    // <CHANGE> Add npm package to Tailwind content scanning
+    "./node_modules/@wiserdomenpm/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
